73fea0fb043dca8615000fa85651cf4cc7f92352
Ryan <kerneltux0@gmail.com>
2020-01-30 14:10:08 -0700

    added to sessions controller added is logged in method for current user verification

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,4 +17,18 @@ class SessionsController < ApplicationController
     end
   end
 
+  def is_logged_in?
+    if logged_in? && current_user
+      render json: {
+        logged_in: true,
+        user: current_user
+      }
+    else
+      render json: {
+        logged_in: false,
+        message: 'no such user'
+      }
+    end
+  end
+
 end
