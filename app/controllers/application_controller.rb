7f67c70daac6346ca1bd93d04f41e4d0a24f50e3
Ryan <kerneltux0@gmail.com>
2020-01-30 14:00:21 -0700

    setup application controller skipped verify authenticity token (not needed for API w/React) setup helper methods for login/logout, verify user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,26 @@
 class ApplicationController < ActionController::Base
+  skip_before_action :verify_authenticiy_token
+
+  helper_method :login!, :logged_in?, :logout!, :current_user, :authorized_user?
+
+  def login!
+    session[:user_id] = @user.id
+  end
+
+  def logged_in?
+    !!session[:user_id]
+  end
+
+  def logout!
+    session.clear
+  end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  
+  def authorized_user?
+    @user == current_user
+  end
+
 end
