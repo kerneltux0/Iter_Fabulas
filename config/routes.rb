5048e60911166d30cb247355c8d9878dad4e9a80
Ryan <kerneltux0@gmail.com>
2020-01-30 14:16:14 -0700

    added to routes file added routes for logging in, logout, and logged-in status

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:create]
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  get '/logged_in', to: 'sessions#is_logged_in?'
 end
