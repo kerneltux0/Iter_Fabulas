85fefd5969e7eccfc67edd0a6566d3cbea900144
Ryan <kerneltux0@gmail.com>
2020-01-31 14:43:56 -0700

    edited sessions controller added create method incl conditional based on successful authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,19 @@
 class SessionsController < ApplicationController
+
+  def create
+    @user = User.find_by(username:session_params[:username])
+    if @user && @user.authenticate(session_params[:password])
+      login!
+      render json: {
+        logged_in: true,
+        user: @user
+      }
+    else
+      render json: {
+        status: 401,
+        error: 'Not a valid user, try again'
+      }
+    end
+  end
+
 end
