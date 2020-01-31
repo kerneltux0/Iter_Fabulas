a2e4db984f9dde172783d33cb4ac7a4b84966be2
Ryan <kerneltux0@gmail.com>
2020-01-31 11:02:22 -0700

    added to Story model tests testing for presence of title, location, and content

--- spec/models/story_spec.rb
+++ spec/models/story_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Story :type => :model do
+  it 'is not valid without a title' do
+    story = Story.create(location:'somewhere', content:'this is what happened', likes:0, comments:[])
+    expect(story).not_to be_valid
+  end
+
+  it 'is not valid without a location' do
+    story = Story.create(title:'something cool', content:'this is what happened', likes:0, comments:[])
+    expect(story).not_to be_valid
+  end
+
+  it 'is not valid without content' do
+    story = Story.create(title:'something awesome', location:'somewhere', likes:0, comments:[])
+    expect(story).not_to be_valid
+  end
   
 end
