3a45ecb5864c62ea6acbec742b8fa53435a2ecd6
Ryan <kerneltux0@gmail.com>
2020-01-31 09:59:39 -0700

    started User model tests wrote tests for presence of username & password

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,18 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   
-  it 'does something' do
-    
+  it 'is valid with username & password' do
+    user = User.create(username: 'username', password: 'password')
+    expect(user).to be_valid
+  end
+
+  it 'is not valid without a password' do
+    user = User.create(username: 'username')
+    expect(user).not_to_be_valid
+  end
+
+  it 'is not valid without a username' do
+    user = User.create(password: 'password')
+    expect(user).not_to_be_valid
   end
 end
