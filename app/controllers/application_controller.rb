8fa97de94389136aff8a0caf80ea21d0fb5f3e68
Ryan <kerneltux0@gmail.com>
2020-01-31 13:11:02 -0700

    edited application controller added skip_before_action due to used as backend API started helper methods for user registration started helper methods for user verification

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+  skip_before_action :verify_authenticity_token
+
+  helper_method :login!, :logged_in?, :logout!, :current_user, :authorized_user?
+
+  def login!
+  end
+
+  def logged_in?
+  end
+
+  def logout!
+  end
+
+  def current_user
+  end
+
+  def authorized_user?
+  end
 end
