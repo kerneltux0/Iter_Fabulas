afa3baf836f0c5015cde53e0820609ca8616280a
Ryan <kerneltux0@gmail.com>
2020-01-31 12:27:38 -0700

    edited Story model tests removed comments parameter array is not valid data type in ActiveRecord

--- spec/models/story_spec.rb
+++ spec/models/story_spec.rb
@@ -2,27 +2,29 @@ require 'rails_helper'
 
 RSpec.describe Story :type => :model do
   it 'is not valid without a title' do
-    story = Story.create(location:'somewhere', content:'this is what happened', likes:0, comments:[])
+    story = Story.create(location:'somewhere', content:'this is what happened', likes:0)
     expect(story).not_to be_valid
   end
 
   it 'is not valid without a location' do
-    story = Story.create(title:'something cool', content:'this is what happened', likes:0, comments:[])
+    story = Story.create(title:'something cool', content:'this is what happened', likes:0)
     expect(story).not_to be_valid
   end
 
   it 'is not valid without content' do
-    story = Story.create(title:'something awesome', location:'somewhere', likes:0, comments:[])
+    story = Story.create(title:'something awesome', location:'somewhere', likes:0)
     expect(story).not_to be_valid
   end
 
   it 'belongs to a user' do
     user = User.create(username:'username', password:'password')
-    story1 = Story.create(title:'something cool', location:'somewhere awesome', content:'this is what happened', likes:0, comments:[])
-    story2 = Story.create(title:'something awesome', location:'on the ocean', content:'let me tell you something', likes:0, comments:[])
+    story1 = Story.create(title:'something cool', location:'somewhere awesome', content:'this is what happened', likes:0)
+    story2 = Story.create(title:'something awesome', location:'on the ocean', content:'let me tell you something', likes:0)
     user.stories << story1
     user.stories << story2
     expect(user.stories.last).to be(story2)
   end
+
+  
   
 end
