5013d895caed28ebcbd3085263798717852fc41d
Ryan <kerneltux0@gmail.com>
2020-01-31 13:41:52 -0700

    edited Story model added validations for title, location & content

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,4 +1,7 @@
 class Story < ApplicationRecord
   belongs_to :user
   has_many :comments
+  validates :title, presence: true
+  validates :content, presence: true
+  validates :location, presence: true
 end
