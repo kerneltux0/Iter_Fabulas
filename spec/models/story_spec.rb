6ac040b82549c132bd44e0c8fbfbc40b55d82228
Ryan <kerneltux0@gmail.com>
2020-01-31 12:33:01 -0700

    adding to Story model tests testing has_many relation with comments

--- spec/models/story_spec.rb
+++ spec/models/story_spec.rb
@@ -22,9 +22,19 @@ RSpec.describe Story :type => :model do
     story2 = Story.create(title:'something awesome', location:'on the ocean', content:'let me tell you something', likes:0)
     user.stories << story1
     user.stories << story2
-    expect(user.stories.last).to be(story2)
+    expect(user.stories.last).to eq(story2)
   end
 
-  
+  it 'has many comments' do
+    story = Story.create(title:'something', location:'somewhere', content:'this is what happened', likes:0)
+    comment1 = story.build_comment(content:'that is great', story_id:story.id)
+    story.comments << comment1
+    story.save
+    comment2 = story.build_comment(content:'whatever, you liar', story_id:story.id)
+    story.comments << comment2
+    story.save
+    expect(story.comments.last).to eq(comment2)
+
+  end
   
 end
