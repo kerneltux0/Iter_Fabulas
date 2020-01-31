e5c0195e2be25be131c9668d60d3b9fb0fcdbc2f
Ryan <kerneltux0@gmail.com>
2020-01-31 13:14:03 -0700

    filled-in helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,17 +4,22 @@ class ApplicationController < ActionController::Base
   helper_method :login!, :logged_in?, :logout!, :current_user, :authorized_user?
 
   def login!
+    session[:user_id]=@user.id
   end
 
   def logged_in?
+    !!session[:user_id]
   end
 
   def logout!
+    session.clear
   end
 
   def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
   def authorized_user?
+    @user == current_user
   end
 end
