11ab5fe2bf57e71cfbc0ed5ec81c0425ceb764d1
Ryan <kerneltux0@gmail.com>
2020-01-31 12:24:58 -0700

    added to User model tests testing for has_many_through relation with comments

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -25,11 +25,29 @@ RSpec.describe User, :type => :model do
 
   it 'has many stories' do
     user = User.create(username:'username', password:'password')
-    story1 = Story.create(title:'something cool', location:'somewhere awesome', content:'this is what happened', likes:0, comments:[])
-    story2 = Story.create(title:'this was ok', location:'nowhere special', content:'I think this was boring', likes:0, comments:[])
+    story1 = Story.create(title:'something cool', location:'somewhere awesome', content:'this is what happened', likes:0)
+    story2 = Story.create(title:'this was ok', location:'nowhere special', content:'I think this was boring', likes:0)
     user.stories << story1
     user.stories << story2
     expect(user.stories.last).to eq(story2)
+  end
+
+  it 'has many comments through stories' do
+    user = User.create(username:'username', password:'password')
+    story = Story.create(title:'something something', location:'somewhere', content:'this is what happened', likes:0)
+    user.stories << story
+    user.story_id = story.id
+    user.save
+    comment1 = story.build_comment(content:'that was great', story_id:story.id)
+    story.comments << comment1
+    story.save
+    comment1.save
+    comment2 = story.build_comment(content:'whatever, you big liar', story_id:story.id)
+    story.comments << comment2
+    story.save
+    comment2.save
+    expect(user.comments.last).to eq(comment2)
+
   end
 
 
