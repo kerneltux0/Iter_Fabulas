0e5aae8bb402f682af1057533b8b49be26f4f5c8
Ryan <kerneltux0@gmail.com>
2020-01-31 14:36:10 -0700

    edited User controller added create method incl conditional based on valid user creation

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,2 +1,17 @@
 class UserController < ApplicationController
+  def create
+    @user = User.create(user_params)
+    if @user.save
+      login!
+      render json: {
+        status: :created,
+        user: @user
+      }
+    else
+      render json: {
+        status: 500,
+        error: @user.errors.full_message
+      }
+    end
+  end
 end
