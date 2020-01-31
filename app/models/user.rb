2be8121cb880fac690a1c589a11ccc92a24d8387
Ryan <kerneltux0@gmail.com>
2020-01-31 13:37:11 -0700

    edited User model added validations for username & password

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
   has_secure_password
   has_many :stories
   has_many :comments, through: :stories
+  validates :username, presence: true
+  validates :username, uniqueness: true
+  validates :password, presence: true
 end
