6f50cfb7e396de98342b4a40709e058e088ab482
Ryan <kerneltux0@gmail.com>
2020-01-31 14:38:09 -0700

    edited user controller added private method for user params

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -14,4 +14,10 @@ class UserController < ApplicationController
       }
     end
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username,:password,:password_confirmation)
+  end
 end
