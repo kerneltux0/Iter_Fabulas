2eb028c7fc5f8b0e17ebbf156027e98c71f0d2ff
Ryan <kerneltux0@gmail.com>
2020-01-31 11:08:23 -0700

    added to Story model tests testing for belongs_to relation to User

--- spec/models/story_spec.rb
+++ spec/models/story_spec.rb
@@ -15,5 +15,14 @@ RSpec.describe Story :type => :model do
     story = Story.create(title:'something awesome', location:'somewhere', likes:0, comments:[])
     expect(story).not_to be_valid
   end
+
+  it 'belongs to a user' do
+    user = User.create(username:'username', password:'password')
+    story1 = Story.create(title:'something cool', location:'somewhere awesome', content:'this is what happened', likes:0, comments:[])
+    story2 = Story.create(title:'something awesome', location:'on the ocean', content:'let me tell you something', likes:0, comments:[])
+    user.stories << story1
+    user.stories << story2
+    expect(user.stories.last).to be(story2)
+  end
   
 end
