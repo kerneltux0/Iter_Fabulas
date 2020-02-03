cf8c2e20bbef49a07443cb414bdf53944e25c924
Ryan <kerneltux0@gmail.com>
2020-02-03 11:14:32 -0700

    updated registrations.js imported Signup & Login components

--- client/src/containers/registrations.js
+++ client/src/containers/registrations.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
+import Signup from '../components/signup';
+import Login from '../components/login';
 
 class Registrations extends Component {
 
   render(){
     return(
       <div>
-        
+
       </div>
     )
   }
