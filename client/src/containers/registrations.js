e270f809ba92652acb3a16ed753d6947360d166c
Ryan <kerneltux0@gmail.com>
2020-02-03 10:52:12 -0700

    created registrations container setup registrations as class container

---
+++ client/src/containers/registrations.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Registrations extends Component {
+
+  render(){
+    return(
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Registrations
