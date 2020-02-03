a19800168090b06c9729f9e49d0292a2354bc8a8
Ryan <kerneltux0@gmail.com>
2020-02-03 10:34:22 -0700

    updated index.js removed unnecessary comments about serviceWorker added imports to utilize Redux added import for reducer

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddelware} from 'redux';
+import thunk from 'thunk';
+import manageStories from './reducer/manageStories';
 import './index.css';
 import App from './App';
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
