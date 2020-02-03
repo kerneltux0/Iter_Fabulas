58ac26e467758a1b0f5ae2bca09ac00eae712cef
Ryan <kerneltux0@gmail.com>
2020-02-03 11:13:46 -0700

    updated app.js fixed typo when importing Registrations container importing BrowserRouer & Route added route for root path

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-import registrations from './containers/registrations';
+import Registrations from './containers/registrations';
+import {BrowserRouter, Route} from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
@@ -7,7 +8,9 @@ class App extends Component {
   render(){
     return(
       <div>
-        
+        <BrowserRouter>
+          <Route exact path='/' component={Registrations} />
+        </BrowserRouter>
       </div>
     )
   }
