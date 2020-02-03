dc7f9a6e1f779e28e331501c18f291237775118b
Ryan <kerneltux0@gmail.com>
2020-02-03 10:51:26 -0700

    updated app.js added import for Component from react added import for registrations container edited App to be class component

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, {Component} from 'react';
+import registrations from './containers/registrations';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+  
+  render(){
+    return(
+      <div>
+        
+      </div>
+    )
+  }
 }
 
 export default App;
