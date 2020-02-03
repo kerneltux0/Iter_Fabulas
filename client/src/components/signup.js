547028315eb49d605fb7113908925960c319c0d5
Ryan <kerneltux0@gmail.com>
2020-02-03 13:07:17 -0700

    updated signup.js setup initial state started signup form

--- client/src/components/signup.js
+++ client/src/components/signup.js
@@ -1,10 +1,21 @@
 import React, {Component} from 'react';
 
 class Signup extends Component {
+  state = {
+    username: '',
+    password: '',
+    password_confirmation: ''
+  }
+
   render(){
     return(
       <div>
-        <p>Signup!</p>
+        <form>
+          <input type='text' value={this.state.username} name='username' placeholder='username' />
+          <input type='password' value={this.state.password} name='password' placeholder='password' />
+          <input type='password' value={this.state.password_confirmation} name='password confirmation' placeholder='confirm password' />
+          <input type='submit' />
+        </form>
       </div>
     )
   }
