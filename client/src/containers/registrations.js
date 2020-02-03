d22a9814d1554abff754e21acd51251680d7893b
Ryan <kerneltux0@gmail.com>
2020-02-03 12:41:29 -0700

    updated registration.js added link to /signup

--- client/src/containers/registrations.js
+++ client/src/containers/registrations.js
@@ -1,13 +1,14 @@
 import React, {Component} from 'react';
 import Signup from '../components/signup';
 import Login from '../components/login';
+import {Link} from 'react-router-dom';
 
 class Registrations extends Component {
 
   render(){
     return(
       <div>
-
+        <Link to='/signup'>Sign Up!</Link>
       </div>
     )
   }
