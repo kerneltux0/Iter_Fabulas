89534f92f31716eb6e11d3f24deb26a5791f6fbb
Ryan <kerneltux0@gmail.com>
2020-02-03 12:47:36 -0700

    updated registrations.js removed unnecessary importing of Signup & Login setup links to signup & login as buttons

--- client/src/containers/registrations.js
+++ client/src/containers/registrations.js
@@ -1,6 +1,4 @@
 import React, {Component} from 'react';
-import Signup from '../components/signup';
-import Login from '../components/login';
 import {Link} from 'react-router-dom';
 
 class Registrations extends Component {
@@ -8,7 +6,12 @@ class Registrations extends Component {
   render(){
     return(
       <div>
-        <Link to='/signup'>Sign Up!</Link>
+        <button>
+          <Link to='/signup'>Sign Up!</Link>  
+        </button>
+        <button>
+          <Link to='/login'>Login!</Link>
+        </button>
       </div>
     )
   }
