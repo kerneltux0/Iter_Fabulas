0d6c923386c14b456958c02b42ffca69b874a33f
Ryan <kerneltux0@gmail.com>
2020-02-05 14:20:23 -0700

    updated addUser.js removed console log's

--- client/src/actions/addUser.js
+++ client/src/actions/addUser.js
@@ -1,12 +1,10 @@
 export function addUser(url,config) {
-  console.log('add user action:',config)
   return(dispatch) => {
     dispatch({type: 'LOADING_USER_ADD'})
     fetch(url,config)
     .then(response => {
       return response.json()
     }).then(responseJSON => {
-      console.log(responseJSON)
       if(responseJSON.error){
         dispatch({type: 'CREATION_ERROR', responseJSON})
       }else{
