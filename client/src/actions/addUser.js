ad13f7601a7b6c6aefc6c8bccbdb196fa2bcf3d2
Ryan <kerneltux0@gmail.com>
2020-02-05 10:01:08 -0700

    updated addUser.js filled-in fetch process for user acct creation currently incomplete (console logging @ end)

--- client/src/actions/addUser.js
+++ client/src/actions/addUser.js
@@ -1,5 +1,12 @@
 export function addUser(url,config) {
   return(dispatch) => {
-
+    dispatch({type: 'LOADING_USER_ADD'})
+    fetch(url,config)
+    .then(response => {
+      return response.json()
+    }).then(responseJSON => {
+      console.log(responseJSON)
+      // dispatch({type: 'CREATE_USER'})
+    })
   }
 }
