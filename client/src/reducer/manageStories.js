b3f2bd429e812253b39c2d1126cdfe85bf60c67f
Ryan <kerneltux0@gmail.com>
2020-02-05 14:03:21 -0700

    updated manageStories.js added logged_in for default state added case statement for loading added case statement for successful creation added case statement for creation error

--- client/src/reducer/manageStories.js
+++ client/src/reducer/manageStories.js
@@ -1,5 +1,26 @@
-const manageStories = (state = {user: {}, stories: [], loading: false}, action) => {
-  return state
+const manageStories = (state = {user: {}, stories: [], loading: false, logged_in: false}, action) => {
+  switch(action.type){
+    case 'LOADING_USER_ADD':
+      return{
+        ...state,
+        user: {...state.user},
+        loading: true
+      }
+    case 'CREATE_USER':
+      return{
+        ...state,
+        user: action.user,
+        loading: false,
+        logged_in: true
+      }
+    case 'CREATION_ERROR':
+      return{
+        message: action.error,
+        loading: false
+      }
+    default:
+      return state
+  }
 }
 
 export default manageStories
