387351b28b285ed07b7dbd8ec876286d0635473a
Ryan <kerneltux0@gmail.com>
2020-02-05 14:38:11 -0700

    updated home.js setup Home as a class component

---
+++ client/src/containers/home.js
@@ -0,0 +1,14 @@
+import {Component} from 'react';
+
+class Home extends Component {
+
+  render(){
+    return(
+      <div>
+        <h1>You're on the home page!</h1>
+      </div>
+    )
+  }
+}
+
+export default Home
