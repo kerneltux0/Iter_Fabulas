30aeefed2aa81f85193c07fbe0ce56ad857574b6
Ryan <kerneltux0@gmail.com>
2020-02-06 14:14:51 -0700

    updated storiesActions.js added action to pull one story

--- client/src/actions/storiesActions.js
+++ client/src/actions/storiesActions.js
@@ -9,3 +9,15 @@ export const fetchStories = () => {
     })
   }
 }
+
+export const fetchStory = (id) => {
+  return(dispatch) => {
+    dispatch({type: 'LOADING_STORIES'})
+    fetch(`http://localhost:3001/stories/${id}`)
+    .then(response => {
+      return response.json()
+    }).then(responseJSON => {
+      dispatch({type: 'FETCH_STORY',story: responseJSON.story})
+    })
+  }
+}
