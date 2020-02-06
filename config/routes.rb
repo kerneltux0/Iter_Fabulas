80f03d594c7be1dca9cdec6d17ec38cf397968c7
Ryan <kerneltux0@gmail.com>
2020-02-06 12:32:21 -0700

    updated routes.rb changed /stories post route to use create method

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'stories#index'
-  post '/stories', to: 'stories#new'
+  post '/stories', to: 'stories#create'
   get '/stories/:id', to: 'stories#show'
   get '/stories/:id/comments', to: 'comments#show'
   post '/stories/:id/comments', to: 'comments#new'
