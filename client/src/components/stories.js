f654cb6cdc3e24884e0e6923305bc03b3638bd71
Ryan <kerneltux0@gmail.com>
2020-02-06 12:07:44 -0700

    updated stories.js mapping through stories prop rendering title as list item

--- client/src/components/stories.js
+++ client/src/components/stories.js
@@ -3,9 +3,15 @@ import React, {Component} from 'react';
 class Stories extends Component {
 
   render(){
+    const storyList = this.props.stories.map(story => {
+      return(<li>{story.title}</li>)
+    })
     return(
       <div>
         <h1>Stories appear here!</h1>
+        <ul>
+          {storyList}
+        </ul>
       </div>
     )
   }
