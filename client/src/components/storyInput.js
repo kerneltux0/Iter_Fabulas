658491924aa554e38b94fc397220ca564184f656
Ryan <kerneltux0@gmail.com>
2020-02-06 12:59:07 -0700

    updated storyInput.js added form for creating a story

--- client/src/components/storyInput.js
+++ client/src/components/storyInput.js
@@ -5,7 +5,17 @@ class StoryInput extends Component {
   render(){
     return(
       <div>
-
+        <form>
+          <input type='text' name='title' value='state.title' placeholder='Story Title' />
+          <br />
+          <input type='text' name='name' value='state.name' placeholder='Your Name' />
+          <br />
+          <input type='text' name='location' value='state.location' placeholder='Story Location' />
+          <br />
+          <textarea name='content' value='state.content' placeholder='Your Story'/>
+          <br />
+          <input type='submit' value='Submit Your Story' />
+        </form>
       </div>
     )
   }
