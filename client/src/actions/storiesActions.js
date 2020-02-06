5c7bc63b588669fd6257977983cd7d26f999fb22
Ryan <kerneltux0@gmail.com>
2020-02-06 11:35:09 -0700

    updated storiesActions.js filled-in dispatching actions for fetchStories

--- client/src/actions/storiesActions.js
+++ client/src/actions/storiesActions.js
@@ -1,3 +1,11 @@
 export const fetchStories = () => {
-  
+  return(dispatch) => {
+    dispatch({type: 'LOADING_STORIES'})
+    fetch('http://localhost:3001')
+    .then(response =>{
+      return response.json()
+    }).then(responseJSON => {
+      dispatch({type: 'ADD_STORIES',stories: responseJSON.stories})
+    })
+  }
 }
