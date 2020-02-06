d509698af23d55d1ab93ddeeb44299324fca5160
Ryan <kerneltux0@gmail.com>
2020-02-06 11:38:53 -0700

    updated manageStories reducer added case for loading stories added case for adding stories

--- client/src/reducer/manageStories.js
+++ client/src/reducer/manageStories.js
@@ -1,6 +1,17 @@
 const manageStories = (state = {stories: [], comments: [], loading: false}, action) => {
   switch(action.type){
-    
+    case 'LOADING_STORIES':
+      return{
+        ...state,
+        stories: [...state.stories],
+        loading: true
+      }
+    case 'ADDING_STORIES':
+      return{
+        ...state,
+        stories: action.stories,
+        loading: false
+      }
     default:
       return state
   }
