400e0376aa9678d4bfbbff5aeebf449a2ce6c319
Ryan <kerneltux0@gmail.com>
2020-02-06 10:30:55 -0700

    updated manageStories.js removed all case statements updated default state

--- client/src/reducer/manageStories.js
+++ client/src/reducer/manageStories.js
@@ -1,23 +1,6 @@
-const manageStories = (state = {user: {}, stories: [], loading: false, logged_in: false}, action) => {
+const manageStories = (state = {stories: [], comments: [], loading: false}, action) => {
   switch(action.type){
-    case 'LOADING_USER_ADD':
-      return{
-        ...state,
-        user: {...state.user},
-        loading: true
-      }
-    case 'CREATE_USER':
-      return{
-        ...state,
-        user: action.user,
-        loading: false,
-        logged_in: true
-      }
-    case 'CREATION_ERROR':
-      return{
-        error: action.error,
-        loading: false
-      }
+    
     default:
       return state
   }
