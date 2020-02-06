f02d9e3355c01938506e8074d24eae96b0271e56
Ryan <kerneltux0@gmail.com>
2020-02-06 13:35:27 -0700

    updated stories.js import Link & Router from react-router-dom import Story component edited return of storyList to be link to indiv. story added route to individual stories

--- client/src/components/stories.js
+++ client/src/components/stories.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
 import StoryInput from './storyInput';
+import {Link, Route, BrowserRouter} from 'react-router-dom';
+import Story from './story';
 class Stories extends Component {
 
   render(){
     const storyList = this.props.stories.map(story => {
-      return(<li>{story.title}</li>)
+      return(<li><Link to={`/stories/${story.id}`}>{story.title}</Link></li>)
     })
     return(
       <div>
@@ -15,6 +17,7 @@ class Stories extends Component {
         <div>
           <StoryInput />
         </div>
+        <Route path={`/stories/:storyId`} component={Story}/>
       </div>
     )
   }
