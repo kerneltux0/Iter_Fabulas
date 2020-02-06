e86b7273443568673b80145037948b71278a7f7e
Ryan <kerneltux0@gmail.com>
2020-02-06 10:44:57 -0700

    updated Comment model test deleted references to user ran model tests

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,10 +3,7 @@ require 'rails_helper'
 RSpec.describe Comment, :type => :model do
   
   it 'is only valid with content' do
-    user = User.create(username:'username', password:'password')
     story = Story.create(title:'something', location:'somewhere', content:'something happened', likes:0)
-    user.stories << story
-    user.save
     comment = Comment.create()
     story.comments << comment
     story.save
