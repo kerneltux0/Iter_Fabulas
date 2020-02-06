2c28bb456c81dd72c498a30f78fe20abe47cafa1
Ryan <kerneltux0@gmail.com>
2020-02-06 11:02:28 -0700

    updated routes.rb added get route for '/' added post route for '/stories' added get route for '/stories/:id' added get route for '/stories/:id/comments' added post route for '/stories/:id/comments'

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/', to: 'stories#index'
+  post '/stories', to: 'stories#new'
+  get '/stories/:id', to: 'stories#show'
+  get '/stories/:id/comments', to: 'comments#show'
+  post '/stories/:id/comments', to: 'comments#new'
 end
