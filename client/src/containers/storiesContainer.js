40e07720ed74aa5638b1f68535189799a05d42bd
Ryan <kerneltux0@gmail.com>
2020-02-06 10:42:37 -0700

    updated storiesContainer.js added rendering for Stories component

--- client/src/containers/storiesContainer.js
+++ client/src/containers/storiesContainer.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
+import Stories from '../components/stories';
 
 class StoriesContainer extends Component {
 
   render() {
     return(
       <div>
-
+        <Stories />
       </div>
     )
   }
