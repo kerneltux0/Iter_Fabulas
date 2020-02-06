e9041fafd0c21b08efcc3ad71fa9cf56dc7f9cc1
Ryan <kerneltux0@gmail.com>
2020-02-06 14:37:37 -0700

    updated storyInput.js defined initial state created event handlers for fields change & form submit added event handler references for inputs & form

--- client/src/components/storyInput.js
+++ client/src/components/storyInput.js
@@ -1,18 +1,35 @@
 import React, {Component} from 'react';
 
 class StoryInput extends Component {
+  state = {
+    title: '',
+    name: '',
+    location: '',
+    content:'',
+    likes: 0
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleSubmit(event){
+    event.preventDefault()
+  }
 
   render(){
     return(
       <div>
-        <form>
-          <input type='text' name='title' value='state.title' placeholder='Story Title' />
+        <form onSubmit={(event)=>this.handleSubmit(event)}>
+          <input onChange={(event)=>this.handleChange(event)} type='text' name='title' value={this.state.title} placeholder='Story Title' />
           <br />
-          <input type='text' name='name' value='state.name' placeholder='Your Name' />
+          <input onChange={(event)=>this.handleChange(event)} type='text' name='name' value={this.state.name} placeholder='Your Name' />
           <br />
-          <input type='text' name='location' value='state.location' placeholder='Story Location' />
+          <input onChange={(event)=>this.handleChange(event)} type='text' name='location' value={this.state.location} placeholder='Story Location' />
           <br />
-          <textarea rows={10} cols={30} name='content' value='state.content' placeholder='Your Story'/>
+          <textarea onChange={(event)=>this.handleChange(event)} rows={10} cols={30} name='content' value={this.state.content} placeholder='Your Story'/>
           <br />
           <input type='submit' value='Submit Your Story' />
         </form>
