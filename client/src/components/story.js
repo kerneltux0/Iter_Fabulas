e9b99c25080e7fec84f6b2055667ea8f0df744ef
Ryan <kerneltux0@gmail.com>
2020-02-10 10:24:10 -0700

    rendering story elements declared variable for props passed-in from Stories component created headings for title, author & location created paragraph for story itself created button for likes

--- client/src/components/story.js
+++ client/src/components/story.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
 const Story = (props) => {
-  console.log(props.location.story)
+  const story = props.location.story.story
 
   return(
     <div>
-      <h1>
-        Story page
-      </h1>
+      <h1>{story.title}</h1>
+      <h2>By: {story.name}</h2>
+      <h4>{story.location}</h4>
+      <p>{story.content}</p>
+      <button>Likes: {story.likes}</button>
     </div>
   )
 }
