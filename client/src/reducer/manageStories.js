567bf8b857b25c017c6511118e20b70c59cbadff
Ryan <kerneltux0@gmail.com>
2020-02-11 10:47:28 -0700

    updated manage stories reducer created case for adding like (start of process) started case for actually increasing likes

--- client/src/reducer/manageStories.js
+++ client/src/reducer/manageStories.js
@@ -12,6 +12,15 @@ const manageStories = (state = {stories: [], comments: [], loading: false}, acti
         stories: [...state.stories],
         loading: true
       }
+    case 'ADDING_LIKE':
+      return{
+        ...state,
+        stories: [...state.stories],
+        loading: true
+      }
+    case 'UPDATING_LIKES':
+      let story = state.stories.find(story => story.id===action.id)
+      
     case 'CREATE_STORY':
       let story = {title: action.title, location: action.location, name: action.name, content: action.content, likes: action.likes}
       return{
