f5a8aa1ef0e9c3aa380f820ff493c173c6839c14
Ryan <kerneltux0@gmail.com>
2020-02-11 10:07:23 -0700

    updated routes.rb removed show route for indiv story (not needed) added patch route for updating likes on indiv story

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'stories#index'
   post '/stories', to: 'stories#create'
-  get '/stories/:id', to: 'stories#show'
+  patch '/stories/:id', to: 'stories#update'
   get '/stories/:id/comments', to: 'comments#show'
   post '/stories/:id/comments', to: 'comments#new'
 end
