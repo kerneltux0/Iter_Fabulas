81fbb2bd24329aa36041737de2f076dc6f0c27fa
Ryan <kerneltux0@gmail.com>
2020-02-12 10:25:16 -0700

    updated story component imported link to add link back to list of stories added story data as argument passed into addLike created button w/link to list of stories added story as argument sent to event handler

--- client/src/components/story.js
+++ client/src/components/story.js
@@ -1,13 +1,14 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom'
 
 class Story extends Component {
 
-  handleLikes(storyId) {
-    this.props.location.addLike(storyId)
+  handleLikes(storyId,storyData) {
+    this.props.location.addLike(storyId,storyData)
   }
 
   render() {
-    const story = this.props.location.story.story
+    const story = this.props.location.story
 
     // const renderComments = story.comments.map(comment => {
     //   return(<p>{comment.content}</p>)
@@ -15,11 +16,12 @@ class Story extends Component {
 
     return(
       <div>
+        <button><Link to={'/'}>Story List</Link></button>
         <h1>{story.title}</h1>
         <h2>By: {story.name}</h2>
         <h4>{story.location}</h4>
         <p>{story.content}</p>
-        <button onClick={this.handleLikes(story.id)}>Likes: {story.likes}</button>
+        <button onClick={this.handleLikes(story.id,story)}>Likes: {story.likes}</button>
         <div>
           {/* {renderComments} */}
         </div>
