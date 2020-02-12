dc26043b5cc6b385c848bae5cf749ebccfbd79a3
Ryan <kerneltux0@gmail.com>
2020-02-12 14:34:53 -0700

    created comments container setup as class component

---
+++ client/src/containers/commentsContainer.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class CommentsContainer extends Component {
+
+  render(){
+    return(
+      <div>
+
+      </div>
+    )
+  }
+}
+
+export default CommentsContainer;
