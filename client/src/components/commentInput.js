c9eb597b3de3613e4c262828b2558959dbaff37b
Ryan <kerneltux0@gmail.com>
2020-02-12 14:26:43 -0700

    updated comment input created basic form setup initial state

--- client/src/components/commentInput.js
+++ client/src/components/commentInput.js
@@ -1,11 +1,17 @@
 import React, {Component} from 'react';
 
 class CommentInput extends Component {
+  state={
+    content: ''
+  }
 
   render() {
     return(
       <div>
-
+        <form>
+          <textarea value='state.content' placeholder='Your comment' />
+          <input type='submit' value='Submit Comment' />
+        </form>
       </div>
     )
   }
