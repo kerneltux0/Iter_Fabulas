7162eea5425e46c6f7c39f38d5ec3b605dce7f22
Ryan <kerneltux0@gmail.com>
2020-02-13 11:17:51 -0700

    updated routes.rb edited route to be more in-line with RESTful convention

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'stories#index'
   post '/stories', to: 'stories#create'
-  get '/stories/:id/comments', to: 'comments#show'
+  get '/stories/:id/comments', to: 'comments#index'
   post '/stories/:id/comments', to: 'comments#create'
 end
