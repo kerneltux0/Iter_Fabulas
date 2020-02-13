2fb9d81fa32ec2acde2a8fee7c3878a94d710bf0
Ryan <kerneltux0@gmail.com>
2020-02-13 13:06:29 -0700

    updated welcome component added line-break between intro & button to see story list

--- client/src/components/welcome.js
+++ client/src/components/welcome.js
@@ -9,7 +9,7 @@ const Welcome = () => {
       <h1>Welcome to Iter Fabulas!</h1>
       <h3 className="iter-intro">
         One of the greatest gifts we can give to each other is share stories of when we took ourselves out of our comfort zones and experienced another culture.  Here you can post & read travel stories, and hopefully develop more of a humanity first perspective.
-      </h3>
+      </h3><br/>
       <button><Link to={'/stories'}>Proceed to Stories</Link></button>
     </div>
   )
