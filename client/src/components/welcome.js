7a19ab38ae6eedbba00a766b4654ab8e4df0c346
Ryan <kerneltux0@gmail.com>
2020-02-13 11:55:51 -0700

    updated welcome component imported Link from react router dom created h1 tag to test rendering added button with Link to /stories

--- client/src/components/welcome.js
+++ client/src/components/welcome.js
@@ -1,6 +1,14 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const Welcome = () => {
+
+  return(
+    <div>
+      <h1>Welcome to this thing</h1>
+      <button><Link to={'/stories'}>Proceed to Stories</Link></button>
+    </div>
+  )
 
 }
 
