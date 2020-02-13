c265a0cb141db4cb66e84051e791ef18bc27abb7
Ryan <kerneltux0@gmail.com>
2020-02-13 12:23:13 -0700

    updated welcome component imported App.css added App class to top-level div

--- client/src/components/welcome.js
+++ client/src/components/welcome.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import '../App.css'
 
 const Welcome = () => {
 
   return(
-    <div>
+    <div className="App">
       <h1>Welcome to this thing</h1>
       <button><Link to={'/stories'}>Proceed to Stories</Link></button>
     </div>
