db99655e5de447c7105a092270b114a91529bb8e
Ryan <kerneltux0@gmail.com>
2020-02-13 09:13:08 -0700

    updated routes changed post route for comments from new to create method

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   get '/', to: 'stories#index'
   post '/stories', to: 'stories#create'
   get '/stories/:id/comments', to: 'comments#show'
-  post '/stories/:id/comments', to: 'comments#new'
+  post '/stories/:id/comments', to: 'comments#create'
 end
