898783a4dbe4ac50dba2f025473bec8644bf2bde
Ryan <kerneltux0@gmail.com>
2020-02-13 12:32:26 -0700

    updated welcome component edited heading to include app name added short description of app added class for styling

--- client/src/components/welcome.js
+++ client/src/components/welcome.js
@@ -6,7 +6,10 @@ const Welcome = () => {
 
   return(
     <div className="App">
-      <h1>Welcome to this thing</h1>
+      <h1>Welcome to Iter Fabulas!</h1>
+      <h3 className="iter-intro">
+        One of the greatest gifts we can give to each other is share stories of when we took ourselves out of our comfort zones and experienced another culture.  Here you can post & read travel stories, and hopefully develop more of a humanity first perspective.
+      </h3>
       <button><Link to={'/stories'}>Proceed to Stories</Link></button>
     </div>
   )
