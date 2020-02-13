863d8d0c4d1b690c8b0277be0f4684c526fb8eab
Ryan <kerneltux0@gmail.com>
2020-02-13 08:38:59 -0700

    updated comments container importing comments component importing comment input component rendering comments component rendering comment input component

--- client/src/containers/commentsContainer.js
+++ client/src/containers/commentsContainer.js
@@ -1,11 +1,14 @@
 import React, {Component} from 'react';
+import Comments from '../components/comments';
+import CommentInput from '../components/commentInput';
 
 class CommentsContainer extends Component {
 
   render(){
     return(
       <div>
-
+        <Comments />
+        <CommentInput />
       </div>
     )
   }
