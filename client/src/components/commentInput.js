0022e3a681eb6e60799c19effe50eef134885496
Ryan <kerneltux0@gmail.com>
2020-02-13 09:28:57 -0700

    updated comment input created event handler for comment submission calling addComment with story id & state (comment content)

--- client/src/components/commentInput.js
+++ client/src/components/commentInput.js
@@ -11,10 +11,15 @@ class CommentInput extends Component {
     })
   }
 
+  handleSubmit(event){
+    event.preventDefault()
+    this.props.addComment(this.props.storyId,this.state)
+  }
+
   render() {
     return(
       <div>
-        <form>
+        <form onSubmit={(event)=>this.handleSubmit(event)}>
           <textarea onChange={(event)=>this.handleChange(event)} name='content' rows={10} cols={30} value={this.state.content} placeholder='Your comment' /><br/>
           <input type='submit' value='Submit Comment' />
         </form>
