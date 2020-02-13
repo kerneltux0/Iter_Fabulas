b07fd511fc52c69657b67cfac85c137f9a8bb212
Ryan <kerneltux0@gmail.com>
2020-02-13 11:13:03 -0700

    updated comments component created lifecycle method to fetch comments created const to map through & render comments

--- client/src/components/comments.js
+++ client/src/components/comments.js
@@ -1,11 +1,21 @@
 import React, {Component} from 'react';
+import Comment from './comment';
 
 class Comments extends Component {
 
+  componentDidMount () {
+    this.props.fetchComments(this.props.story.id)
+  }
+
+
   render() {
+    const renderComments = this.props.comments.map(comment => {
+      return(<Comment comment={comment}/>)
+    })
+
     return(
       <div>
-
+        {renderComments}
       </div>
     )
   }
