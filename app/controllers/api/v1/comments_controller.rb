898679f78a0d31071c3fc71401fb36268e9a8995
Ryan <kerneltux0@gmail.com>
2020-03-05 11:21:09 -0700

    updated controllers created api/v1 directory moved comments & stories controllers into that directory created Api::V1 module around controllers for namespacing

---
+++ app/controllers/api/v1/comments_controller.rb
@@ -0,0 +1,30 @@
+module Api::V1  
+  class CommentsController < ApplicationController
+
+    def create
+      @story = Story.find(params[:id])
+      @comment = Comment.new(comment_params)
+      @story.comments << @comment
+      @comment.save
+      @story.save
+      render json: {
+        comment: @comment
+      }
+
+    end
+
+    def index
+      @story = Story.find(params[:id])
+      render json: {
+        comments: @story.comments
+      }
+
+    end
+
+    private
+    def comment_params
+      params.require(:comment).permit(:content)
+    end
+
+  end
+end
---
+++ app/controllers/api/v1/stories_controller.rb
@@ -0,0 +1,27 @@
+module Api::V1
+  class StoriesController < ApplicationController
+
+    def index
+      @stories = Story.all
+      render json: {
+        stories: @stories
+      }
+    end
+
+    def create
+      @story = Story.new(story_params)
+      if @story.save
+        render json: {
+          story: @story
+        }
+      end
+
+    end
+
+    private
+      def story_params
+        params.require(:story).permit(:title,:name,:location,:content,:likes)
+      end
+
+  end
+end
