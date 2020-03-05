30f9a6bedaf0c221022d7c0ad0031e02088bea6f
Ryan <kerneltux0@gmail.com>
2020-03-05 11:18:46 -0700

    updated rails routes added api/v1 namespacing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/', to: 'stories#index'
-  post '/stories', to: 'stories#create'
-  get '/stories/:id/comments', to: 'comments#index'
-  post '/stories/:id/comments', to: 'comments#create'
+  namespace :api do
+    namespace :v1 do
+      get '/', to: 'stories#index'
+      post '/stories', to: 'stories#create'
+      get '/stories/:id/comments', to: 'comments#index'
+      post '/stories/:id/comments', to: 'comments#create'
+    end
+  end
+  
 end
