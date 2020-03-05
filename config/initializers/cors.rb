42a06c68707e664d5dbb19695b9b5661da079b36
Ryan <kerneltux0@gmail.com>
2020-03-05 13:56:50 -0700

    attempting to resolve cors errors altered origins in cors initializer to heroku URL removed http's in AJAX calls in actions

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:5000'
+    origins 'https://iter-fabulas.herokuapp.com'
     resource '*',
       headers: :any,
       methods: [:get,:post,:put,:patch,:delete,:options,:head]
