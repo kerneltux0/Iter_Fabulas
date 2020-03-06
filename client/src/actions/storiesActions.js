62b3d198052468326e91e625672c6be0dcbc98f7
Ryan <kerneltux0@gmail.com>
2020-03-06 09:14:04 -0700

    attempting to fix issues on live site changed fetch request to http

--- client/src/actions/storiesActions.js
+++ client/src/actions/storiesActions.js
@@ -1,7 +1,7 @@
 export const fetchStories = () => {
   return(dispatch) => {
     dispatch({type: 'LOADING_STORIES'})
-    fetch('https://localhost:3001/api/v1/')
+    fetch('http://localhost:3001/api/v1/')
     .then(response =>{
       console.log(response)
       return response.json()
